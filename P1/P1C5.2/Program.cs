7aa61a18b2ee0540e87a4b812602156a0ca137d6
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:43:46 +0200

    feat: adding P1C5.2

---
+++ P1/P1C5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿// TODO : Remplacer le ?? par le code approprié pour créer une liste de chaînes
+
+IList <??> invites = new ?? ();
+
+// TODO : Ajouter Joe, Martin et Marie à la liste.
+
+
+// TODO : Compléter l'instruction suivante en remplaçant le ?? pour afficher la taille de la liste
+Console.WriteLine(invites.??);
+
+// TODO : Remplacer Martin par Jean dans la liste
+
+
+// TODO : Retirer Joe de la liste
+
+
+// Afficher le contenu de la liste
+Console.WriteLine("Les invités sont :");
+foreach (string invite in invites)
+{
+    Console.WriteLine(invite);
+}
---
+++ P1/P1C5.2/README.md
@@ -0,0 +1,12 @@
+﻿# Manipulez une liste d'invités
+Remplacez le ?? par le code correct dans l'instruction IList<??> invites = new ??();.
+
+Ajoutez 3 invités à la liste : Joe, Martin et Marie.
+
+Affichez la taille de la liste.
+
+Remplacez Martin par Jean dans la liste.
+
+Supprimez Joe de la liste.
+
+Exécutez le code pour afficher le résultat final.
