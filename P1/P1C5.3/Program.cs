bfec64f995f2ba1e70d1c08b8c35e243566374d9
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:48:51 +0200

    feat: adding P1C5.3

---
+++ P1/P1C5.3/Program.cs
@@ -0,0 +1,18 @@
+﻿ISet<string> ingredients = new HashSet<string>();
+ingredients.Add("sucre");
+ingredients.Add("chocolat");
+ingredients.Add("beurre");
+ingredients.Add("vanille");
+
+// TODO : ajouter un autre ingrédient à l'ensemble
+
+
+// TODO : retirer la vanille de l'ensemble
+
+
+// Afficher la liste des ingrédients
+Console.WriteLine("Voici la liste de nos ingrédients");
+foreach (String ingredient in ingredients)
+{
+    Console.WriteLine(ingredient);
+}
---
+++ P1/P1C5.3/README.md
@@ -0,0 +1,6 @@
+﻿# Manipulez un ensemble
+Ajoutez un autre ingrédient à l'ensemble.
+
+Supprimez la vanille.
+
+Exécutez le code pour afficher le résultat.
