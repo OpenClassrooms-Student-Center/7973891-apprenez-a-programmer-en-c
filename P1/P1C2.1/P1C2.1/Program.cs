6e00997476a8b614c200b6cd297df9f8390582ac
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 12:52:14 +0200

    feat: adding P1C2.1

---
+++ P1/P1C2.1/P1C2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// déclarer une variable int nommée allocationCourante
+int allocationCourante = 200;
+// déclarer une variable int epargne
+int epargne = 3000;
+
+// TODO : créer une variable int nommée prime avec pour valeur initiale 500.
+
+
+// Afficher le résultat
+Console.WriteLine("Votre allocation courante est de " + allocationCourante);
+Console.WriteLine("Le montant de votre épargne est de " + epargne);
+Console.WriteLine("Vous bénéficiez d'une prime de " + prime);
---
+++ P1/P1C2.1/P1C2.1/README.md
@@ -0,0 +1,5 @@
+﻿# Déclarez votre première variable 
+
+Dans la classe Program.cs, déclarez une variable de type int nommée prime avec la valeur 500 en dessous du commentaire TODO.
+
+Cliquez ensuite sur le bouton d'exécution. Si le code est correct, les valeurs s'afficheront !
