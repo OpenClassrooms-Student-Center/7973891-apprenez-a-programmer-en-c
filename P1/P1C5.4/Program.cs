60201426e2c5e6a6e62510bb4e7a987fdbe69fc4
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:51:58 +0200

    feat: Adding P1C5.4

---
+++ P1/P1C5.4/Program.cs
@@ -0,0 +1,19 @@
+﻿IDictionary<string, int> mois = new Dictionary<string, int>();
+
+// TODO : Remplacer les noms par des constantes de type chaine de caractères
+mois.Add("Juin", 6);
+mois.Add("Septembre", 9);
+mois.Add("Mars", 5);
+
+// TODO : Corriger la valeur de Mars avec (3)
+
+
+// TODO : Retirer Juin
+
+
+// Afficher le contenu du dictionnaire
+Console.WriteLine("La liste des mois est :");
+foreach (KeyValuePair<string, int> clePaire in mois)
+{
+    Console.WriteLine(clePaire.Key.ToString() + " est le numéro du mois de " + clePaire.Value + " de l'année ");
+}
---
+++ P1/P1C5.4/README.md
@@ -0,0 +1,5 @@
+﻿# Manipulez les dictionnaires
+Remplacez les noms par des variables constantes .
+Corrigez mars en indiquant sa valeur correcte (3).
+Supprimez juin du dictionnaire.
+Exécutez le code pour afficher le contenu du dictionnaire.
