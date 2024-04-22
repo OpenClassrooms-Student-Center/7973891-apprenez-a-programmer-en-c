5bdaf21e457ca36e76dd26dc602aa3e09b78913a
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:22:23 +0200

    feat: adding P1C3.1

---
+++ P1/P1C3.1/Program.cs
@@ -0,0 +1,8 @@
+﻿int numerateur = 10;
+int denominateur = 4;
+
+// TODO : modifier l'instruction ci-dessous pour que la réponse prenne une valeur décimale
+int reponse = numerateur / denominateur;
+
+// Afficher le résultat 
+Console.WriteLine(numerateur + " / " + denominateur + " = " + reponse);
---
+++ P1/P1C3.1/README.md
@@ -0,0 +1,2 @@
+﻿# Mélangez des types numériques
+Modifiez l'instruction int reponse = numerateur/denominateur; pour que reponse prenne la valeur décimale correcte.
