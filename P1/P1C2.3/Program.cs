c5a84084482f005c2fac957bf94ef16643ce05c8
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:16:20 +0200

    feat: adding P1C2.3

---
+++ P1/P1C2.3/Program.cs
@@ -0,0 +1,12 @@
+﻿// TODO, étape 1 :
+// Créer une variable nommée jourDeDepart avec pour valeur initiale 3
+
+// TODO, étape 2 :
+// Créer une constante nommée joursDansSemaine avec pour valeur 7
+
+// TODO, étape 3 :
+// Utiliser un opérateur de raccourci pour ajouter la valeur de la constante joursDansSemaine à la variable jourDeDepart
+
+// Afficher le résultat 
+Console.WriteLine("Il y a " + joursDansSemaine + " jours dans la semaine.");
+Console.WriteLine("Votre jour de départ du mois est : " + jourDeDepart);
---
+++ P1/P1C2.3/README.md
@@ -0,0 +1,8 @@
+﻿# Instructions
+Créez une variable jourDeDepart avec une valeur de 3.
+
+Créez une constante int nommée joursDansSemaine et affectez-lui la valeur de 7.
+
+Utilisez un opérateur mathématique raccourci pour ajouter la valeur de la variable joursDansSemaine à la variable jourDeDepart.
+
+Exécutez le code !
