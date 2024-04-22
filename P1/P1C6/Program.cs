252ad7482bf42437942a74fc306c86955983abfb
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 14:56:00 +0200

    feat: adding P1C6

---
+++ P1/P1C6/Program.cs
@@ -0,0 +1,10 @@
+﻿string racine = "Je suis accessible par toutes les lignes de code dans mon contexte";
+
+string espion = "Je suis un espion";
+// TODO : Supprimer les accolades « { } »
+{
+    string entreAccolades = "Je suis un infiltré";
+}
+Console.WriteLine(racine);
+Console.WriteLine(espion);
+Console.WriteLine(entreAccolades);
---
+++ P1/P1C6/README.md
@@ -0,0 +1,4 @@
+﻿# Contrôlez la portée
+Exécutez le code. Que dit le message d'erreur après la commande de compilation ?
+Supprimez les accolades autour de la variable entreAccolades.
+Exécutez à nouveau le code.
