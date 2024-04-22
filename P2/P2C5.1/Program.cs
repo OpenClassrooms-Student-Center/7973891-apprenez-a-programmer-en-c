6ae7a57972c028901bc2aa71a1cd77d58716f985
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-22 16:27:55 +0200

    feat: adding P2C5.1

---
+++ P2/P2C5.1/Program.cs
@@ -0,0 +1,66 @@
+﻿string chaineUrl = "";
+int nombreArguments;
+
+if (args == null)
+{
+    nombreArguments = 0;
+}
+else
+{
+    nombreArguments = args.Length;
+}
+
+// affecter une valeur à chaineUrl ou quitter
+switch (nombreArguments)
+{
+    case 0:
+        chaineUrl = DemanderUneUrl();
+        break;
+    case 1:
+        chaineUrl = args[0];
+        break;
+    default:
+        Console.WriteLine("Veuillez exécuter le programme avec une URL ou rien du tout");
+        Environment.Exit(-1);
+        break;
+}
+
+Console.WriteLine("Vérification de l'URL " + chaineUrl);
+
+if (URLValide(chaineUrl))
+{
+    Console.WriteLine(chaineUrl + " est une URL valide");
+}
+else
+{
+    Console.WriteLine(chaineUrl + " n'est pas une URL valide");
+}
+
+
+/// <summary>
+/// Demander à l'utilisateur de saisir une URL
+/// </summary>
+/// <returns>Demander à l'utilisateur de saisir une URL</returns>
+static string DemanderUneUrl()
+{
+    Console.WriteLine("Veuillez saisir une URL valide");
+    string url = Console.ReadLine();
+    return url;
+}
+
+/// <summary>
+/// Vérifier le formatage d'une URL
+/// </summary>
+/// <param name="chaineUrl"></param>
+/// <returns>vrai si le format correspond a une URL, sinon faux</returns>
+static bool URLValide(string chaineUrl)
+{
+    if (Uri.IsWellFormedUriString(chaineUrl, UriKind.Absolute))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
---
+++ P2/P2C5.1/README.md
@@ -0,0 +1,10 @@
+﻿# Instructions
+Cliquez sur le bouton vert Run pour compiler et exécuter l'application.
+Le système vous demande de saisir une URL valide.
+Saisissez dans le terminal ou console Powershell dans Visual Studio, "dotnet run https://www.openclassrooms.com. La sortie devrait afficher :
+Vérification de l'URL https://www.openclassrooms.com
+https://www.openclassrooms.com est une URL valide.
+Essayez une URL non valide. 
+Saisissez dans le terminal ou console Powershell dans Visual Studio, "dotnet run EstCeUneUrlValide". La sortie devrait afficher :
+Vérification de l'URL EstCeUneUrlValide
+EstCeUneUrlValide n'est pas une URL valide
