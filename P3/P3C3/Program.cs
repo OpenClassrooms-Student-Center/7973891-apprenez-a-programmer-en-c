91dc588c8e6843ac64acf60e7281728f06a58278
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-24 16:41:27 +0200

    refactor: refactoring P3C3

--- P3/P3C3/Program.cs
+++ P3/P3C3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Essayons une addition !");
+int x = 1;
+int y = 2;
+int z = x + y;
+Console.WriteLine("La somme est de " + z);
+
+Console.WriteLine("Essayons une addition !");
+int i = 3;
+int j = 4;
+int k = i + j;
+Console.WriteLine("La somme est de " + k);
+
+Console.WriteLine("Essayons une addition !");
+int var1 = 5;
+int var2 = 6;
+int var3 = var1 + var2;
+Console.WriteLine("La somme est de " + var3);
