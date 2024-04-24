3e526fd2b7345376678c0494264f659975f8d770
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-24 15:28:58 +0200

    refactor: refactoring P1C5.1

--- P1/P1C5.1/Program.cs
+++ P1/P1C5.1/Program.cs
@@ -1,4 +1,4 @@
-﻿// TODO : Déclarer une variable nommée couleurs d'un array de chaînes de longueur 5
+﻿// TODO : Déclarer une variable nommée couleurs d'un array de string de longueur 5
 
 
 // TODO : Remplir le tableau avec les couleurs demandées dans le README.md
@@ -8,7 +8,7 @@
 
 
 // Afficher le contenu du tableau
-foreach (String couleur in couleurs)
+foreach (string couleur in couleurs)
 {
     Console.WriteLine(couleur);
 }
